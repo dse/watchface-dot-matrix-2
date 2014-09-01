f650a546b9c2fbc95681679c6924a460f28b19f1
Darren Embry <dse@webonastick.com>
2014-09-01 19:34:37 -0400

    add largerClockFont; genericize.

--- configpage/config.js
+++ configpage/config.js
@@ -20,22 +20,36 @@ console_log("This is config.js.  location.href = " + JSON.stringify(location.hre
 	});
 }(jQuery));
 
+var CONFIG_OPTIONS = [
+	{ name: "blackOnWhite",    type: "boolean" },
+	{ name: "showDate",        type: "boolean" },
+	{ name: "showBattery",     type: "boolean" },
+	{ name: "largerClockFont", type: "boolean" }
+];
+
 jQuery(function($) {
 	console_log("This is config.js's ready handler, executed via jQuery(function($) { ... }).");
 
 	var $form = $("form.configForm");
-	var $cb_blackOnWhite = $form.find(":checkbox[name='blackOnWhite']");
-	var $cb_showDate     = $form.find(":checkbox[name='showDate']");
-	var $cb_showBattery  = $form.find(":checkbox[name='showBattery']");
+
+	var $cb;
+	CONFIG_OPTIONS.forEach(function(option) {
+		if (option.type === "boolean") {
+			$cb[option.name] = $form.find(":checkbox[name='" + option.name + "']");
+		}
+	});
+
 	var $b_cancel = $form.find("#b-cancel");
 	var $b_submit = $form.find("#b-submit");
+
 	function saveOptions() {
-		return {
-			blackOnWhite: $cb_blackOnWhite.is(":checked") ? true : false,
-			showDate:     $cb_showDate    .is(":checked") ? true : false,
-			showBattery:  $cb_showBattery .is(":checked") ? true : false
-		};
+		var result = {};
+		CONFIG_OPTIONS.forEach(function(option) {
+			result[option.name] = $cb[option.name].is(":checked") ? true : false;
+		});
+		return result;
 	}
+
 	function param(name) {
 		var rx = new RegExp("(?:^|\\?|\\&)" + name + "=(.*?)(?:$|\\&)");
 		var match = rx.exec(location.search);
@@ -56,25 +70,15 @@ jQuery(function($) {
 	$b_submit.click(function() {
 		document.location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(saveOptions()));
 	});
-	
-	var blackOnWhite = param("blackOnWhite");
-	var showDate     = param("showDate");
-	var showBattery  = param("showBattery");
 
-	console_log("config.js: blackOnWhite = " + JSON.stringify(blackOnWhite));
-	console_log("config.js: showDate     = " + JSON.stringify(showDate));
-	console_log("config.js: showBattery  = " + JSON.stringify(showBattery));
-	
-	if (Number(blackOnWhite) || blackOnWhite === "true") {
-		console_log("A");
-		$cb_blackOnWhite.attr("checked", true).checkboxradio("refresh");
-	}
-	if (Number(showDate) || showDate === "true") {
-		console_log("B");
-		$cb_showDate.attr("checked", true).checkboxradio("refresh");
-	}
-	if (Number(showBattery) || showBattery === "true") {
-		console_log("C");
-		$cb_showBattery.attr("checked", true).checkboxradio("refresh");
-	}
+	var options = {};
+
+	CONFIG_OPTIONS.forEach(function(option) {
+		options[option.name] = param(option.name);
+		if (option.type === "boolean") {
+			if (Number(options[option.name]) || options[option.name] === "true") {
+				$cb[option.name].attr("checked", true).checkboxradio("refresh");
+			}
+		}
+	});
 });
